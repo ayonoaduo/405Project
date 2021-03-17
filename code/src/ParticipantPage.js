8790603a903151480935826831d973c480ccca1c
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2021-03-17 00:42:52 -0600

    More code

---
+++ code/src/ParticipantPage.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import { auth } from "./firebase";
+function ParticipantPage({ user, setOpenSignIn }) {
+  return (
+    <div>
+      <nav
+        class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top"
+        id="mainNav"
+      >
+        <div class="container">
+          <a class="navbar-brand js-scroll-trigger" href="#page-top">
+            Project Vitality
+          </a>
+          <button
+            class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+            type="button"
+          >
+            Menu
+            <i>
+              <MenuIcon style={{ fontSize: 20 }} />
+            </i>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item mx-0 mx-lg-1">
+                {user?.displayName ? ( //if the user exists, show a Logout button
+                  <div className="app__loginContainer">
+                    <button
+                      className="border__Button nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger text-uppercase font-weight-bold  "
+                      onClick={() => auth.signOut()}
+                    >
+                      Logout
+                    </button>
+                  </div>
+                ) : (
+                  //else, show a sign up button
+                  <div className="app__loginContainer">
+                    <button
+                      className="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger text-uppercase font-weight-bold "
+                      onClick={() => setOpenSignIn(true)}
+                    >
+                      Sign In
+                    </button>
+                    {/* <button
+                      class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger text-uppercase font-weight-bold  "
+                      onClick={() => setOpen(true)}
+                    >
+                      Sign Up
+                    </button> */}
+                  </div>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default ParticipantPage;
