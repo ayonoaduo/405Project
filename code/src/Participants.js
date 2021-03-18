88acf1945efeb9b65cc4f90a82136d91f7e0b872
Onisokien Ayonoadu <ayonoaduo@gmail.com>
2021-03-17 22:49:27 -0600

    Participant List & Page

--- code/src/Participants.js
+++ code/src/Participants.js
@@ -1,13 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Participants.css";
-function Participants() {
+import { db } from "./firebase";
+function Participants({ userId, user, name, address }) {
   return (
-    <div>
-      <div className="participants">
-        <div className="participants__styling">
-          <h5>Participants</h5>
-        </div>
-      </div>
+    <div className="participants">
+      <button className="text-uppercase font-weight-bold bg-primary text-white rounded">
+        {name}
+      </button>
     </div>
   );
 }
